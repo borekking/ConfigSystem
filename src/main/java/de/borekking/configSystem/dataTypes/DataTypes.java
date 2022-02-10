bca5379135e7b3168ddc3efd47e72c85e7574910
borekking <mcnairolfflo@gmail.com>
2022-02-10 18:33:50 +0100

    Added fake DataTypes enum

---
+++ src/main/java/de/borekking/configSystem/dataTypes/DataTypes.java
@@ -0,0 +1,29 @@
+package de.borekking.configSystem.dataTypes;
+
+public class DataTypes {
+
+    /*
+     * Own Enumeration for all IDataTypes.
+     * (Because normal enumerations don't allow Generic.)
+     *
+     */
+
+    public final static IDataType<Boolean> BOOLEAN = new BooleanDataType();
+
+    public final static IDataType<Byte> BYTE = new ByteDataType();
+
+    public final static IDataType<Character> CHAR = new CharacterDataType();
+
+    public final static IDataType<Short> SHORT = new ShortDataType();
+
+    public final static IDataType<Integer> INTEGER = new IntegerDataType();
+
+    public final static IDataType<Long> Long = new LongDataType();
+
+    public final static IDataType<Float> FLOAT = new FloatDataType();
+
+    public final static IDataType<Double> DOUBLE = new DoubleDataType();
+
+    public final static IDataType<String> STRING = new StringDataType();
+
+}
