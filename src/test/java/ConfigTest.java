0ca8948f39035c11d2af28dd43303b1a7a02c8aa
borekking <mcnairolfflo@gmail.com>
2022-02-10 17:29:20 +0100

    Single imports!

--- src/test/java/ConfigTest.java
+++ src/test/java/ConfigTest.java
@@ -1,5 +1,9 @@
 import de.borekking.configSystem.Config;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
